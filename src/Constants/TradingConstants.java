e575c1fecbd87381bfdd90f112b58ae7432ff754
Nallamilli Reddy S G B Rama Krishna Reddy <nallamilli.reddy@bounteous.com>
2026-03-11 11:38:09 +0530

    Added Constants.

---
+++ src/Constants/TradingConstants.java
@@ -0,0 +1,21 @@
+package Constants;
+
+public final class TradingConstants {
+
+    private TradingConstants() {}
+
+    public static final int THREAD_POOL_SIZE = 10;
+
+    public static final int TOTAL_TRADES = 100;
+
+    public static final int MAX_QUANTITY = 20;
+
+    public static final String[] USERS = {
+            "User1","User2","User3","User4","User5"
+    };
+
+    public static final String[] STOCKS = {
+            "AAPL","GOOG","TSLA"
+    };
+
+}
